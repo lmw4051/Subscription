1db70c28535fccedcf1e78d9ed05eee36707bca2
David Lee <david@Davids-MacBook-Pro.local>
2022-11-21 17:34:32 +0800

    Initial Commit

---
+++ Subscription/SubscriptionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubscriptionApp.swift
+//  Subscription
+//
+//  Created by David Lee on 11/21/22.
+//
+
+import SwiftUI
+
+@main
+struct SubscriptionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
